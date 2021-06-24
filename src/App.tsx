ae5b4ef96ecc8242e254277511c40637c636247d
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-06-24 03:37:19 -0300

    feat(routes): implement router and create home and new-room routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
