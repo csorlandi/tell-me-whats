2e36804f917f51f63fa62743f61ef289ddbd14e3
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-06-24 14:48:14 -0300

    chore(room-code): implement copy room code to clipboard

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -2,13 +2,21 @@ import copyImg from '../../assets/images/copy.svg';
 
 import './style.scss';
 
-export function RoomCode() {
+type RoomCodeProps = {
+  code: string;
+};
+
+export function RoomCode({ code }: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(code);
+  }
+
   return (
-    <button className="room-code">
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #-McynOYVmmVAHHa_fkSP</span>
+      <span>Sala #{code}</span>
     </button>
   )
 }
--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,3 +1,5 @@
+import { useParams } from 'react-router-dom';
+
 import { Button } from '../../components/Button';
 import { RoomCode } from '../../components/RoomCode';
 
@@ -5,13 +7,19 @@ import logoImg from '../../assets/images/logo.svg';
 
 import './style.scss';
 
+type RoomParams = {
+  id: string;
+};
+
 export function Room() {
+  const params = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Tellmewhats" />
-          <RoomCode />
+          <RoomCode code={params.id} />
         </div>
       </header>
 
