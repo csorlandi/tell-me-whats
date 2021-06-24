ef6342411a358a2edf06b6ffed51bb3fa9e4b6ae
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-06-24 03:31:23 -0300

    feat(new-room): implement new room page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <Home />
+    <NewRoom />
   );
 }
 
