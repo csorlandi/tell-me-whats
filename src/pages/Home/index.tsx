8abaf1de20831319bb4683555ee4509a3412d098
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-06-24 03:43:06 -0300

    chore(routes): implement navigation between home and new-room through buttons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,20 @@
+import { useHistory } from 'react-router-dom';
+
+import { Button } from '../../components/Button';
+
 import illustrationImg from '../../assets/images/illustration.svg';
 import logoImg from '../../assets/images/logo.svg';
 import googleIconImg from '../../assets/images/google-icon.svg';
 
 import './style.scss';
-import { Button } from '../../components/Button';
 
 export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new');
+  }
+
   return  (
     <div id="page-auth">
       <aside>
@@ -16,7 +25,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Tellmewhats" />
-          <button className="create-room">
+          <button className="create-room" onClick={navigateToNewRoom}>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../../assets/images/illustration.svg';
 import logoImg from '../../assets/images/logo.svg';
 
@@ -22,7 +24,7 @@ export function NewRoom() {
               Criar sala
             </Button>
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">clique aqui</a></p>
+          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
         </div>
       </main>
     </div>
