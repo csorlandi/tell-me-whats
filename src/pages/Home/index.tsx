140c5a68ce58a60a58370725db99df08fe0145d4
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-06-24 03:15:56 -0300

    style(home): create style to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,22 +2,24 @@ import illustrationImg from '../../assets/images/illustration.svg';
 import logoImg from '../../assets/images/logo.svg';
 import googleIconImg from '../../assets/images/google-icon.svg';
 
+import './style.scss';
+
 export function Home() {
   return  (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Tellmewhats" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input type="text" placeholder="Digite o código da sala" />
             <button type="submit">Entrar na sala</button>
