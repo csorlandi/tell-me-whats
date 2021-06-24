7c4f1e89ebb5b432952124c6abbec8c023ae7264
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-06-24 03:50:04 -0300

    feat(auth): implement google authentication with firebase

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom';
 
+import { auth, firebase } from '../../services/firebase';
+
 import { Button } from '../../components/Button';
 
 import illustrationImg from '../../assets/images/illustration.svg';
@@ -11,8 +13,14 @@ import './style.scss';
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+
+      history.push('/rooms/new');
+    })
   }
 
   return  (
@@ -25,7 +33,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Tellmewhats" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
