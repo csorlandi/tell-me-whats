0bd9c4b6f435549872b3a84ee4bb6fa52624a27c
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-06-26 00:24:23 -0300

    feat(question-buttons): implement answer and highlight question buttons

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -9,6 +9,8 @@ import { Question } from '../../components/Question';
 
 import logoImg from '../../assets/images/logo.svg';
 import deleteImg from '../../assets/images/delete.svg';
+import checkImg from '../../assets/images/check.svg';
+import answerImg from '../../assets/images/answer.svg';
 
 import './style.scss';
 
@@ -29,6 +31,18 @@ export function AdminRoom() {
     });
 
     history.push('/');
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
   }
 
   async function handleDeleteQuestion(questionId: string) {
@@ -64,6 +78,18 @@ export function AdminRoom() {
               content={question.content}
               author={question.author}
             >
+              <button
+                type="button"
+                onClick={() => handleCheckQuestionAsAnswered(question.id)}
+              >
+                <img src={checkImg} alt="Marcar Pergunta como respondida" />
+              </button>
+              <button
+                type="button"
+                onClick={() => handleHighlightQuestion(question.id)}
+              >
+                <img src={answerImg} alt="Destacar Pergunta" />
+              </button>
               <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
                 <img src={deleteImg} alt="Remover Pergunta" />
               </button>
